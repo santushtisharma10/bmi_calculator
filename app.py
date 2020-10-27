f765f8f8c9012798b7be0290d9d0fb96eb376fd4
santushtisharma10 <sharmasantushti1012@gmail.com>
2020-10-27 23:23:19 +0530

    app.py changed

--- app.py
+++ app.py
@@ -2,9 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def calc():
+    bmi = weight/(height**2)
+    return bmi
+
+def checkHealth():
+
 @app.route("/")
 def main():
     return render_template("index.html")
+  
+@app.route("/form", method={"GET", "POST"})
+def form():
+    return render_template("result.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
